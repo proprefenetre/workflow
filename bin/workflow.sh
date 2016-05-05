a2750162b121a33483f9354415409195cb4807e5
proprefenetre <nielseigenraam@gmail.com>
2016-05-05 15:58:42 +0200

    check for dir name

--- bin/workflow.sh
+++ bin/workflow.sh
@@ -7,8 +7,14 @@ GIGNORE=.gitignore
 TEMPLATE=project.md
 REFS=refs.bib
 
-[[ ! -d .git ]] && git init
 
-cp -r $WFDIR/* .
-
-mv project.md ${PWD##*/}.md
+if [[ $# -eq 0 ]]; then
+    echo "No arguments supplied" 
+    exit 1
+else
+    [[ ! -d ./$1 ]] && mkdir -p ./$1
+    cd ./$1
+    [[ ! -d .git ]] && git init
+    cp -r $WFDIR/* .
+    mv project.md ${PWD##*/}.md
+fi
